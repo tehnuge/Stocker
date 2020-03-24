c4ebbd707e6c844e4eafde51f3cbe221dcc1794c
John Duong <jhnduong@gmail.com>
2020-03-23 22:29:21 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import https from 'https';
+
+const key = '34BY34YZCJX32Y67';
+const sym = 'MSFT';
+const interval = '5min';
+const func = 'TIME_SERIES_INTRADAY';
+const url = `https://www.alphavantage.co/query?function=${func}&symbol=${sym}&interval=${interval}&apikey=${key}`;
 
 function App() {
+  const [stocks, setStocks] = useState([]);
+
+
+const processResponse = (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    setStocks(data);
+
+    //console.log(JSON.parse(data).explanation);
+  });
+
+};
+
+  useEffect(() => {
+    https.get(url, processResponse).on("error", (err) => {
+    console.log('NO!')
+
+  console.log("Error: " + err.message);
+});
+}, []);
+
+  console.log(stocks);
   return (
     <div className="App">
       <header className="App-header">
